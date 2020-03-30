7b31733dbd6b4869d8a789ed90349f2fbe7598b3
zepcp <vieirapereira@protonmail.com>
2020-03-30 14:40:44 +0100

    add changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -2,14 +2,14 @@
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "4934c2de71727ef1c40d7f0b23371e13",
-"/main.dart.js": "0e3e2677f2f561bd6f2e6c6fe9568c60",
+"/main.dart.js": "00198b99ff1f616c505284029eca4f4c",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "73974f27b05f20b4cb13d13177ec9f50",
 "/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/LICENSE": "6366f81b977ddc1c2a551bf32bbd9518",
+"/assets/LICENSE": "c58d6377535c44c30d5f38e745eb6cb4",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
