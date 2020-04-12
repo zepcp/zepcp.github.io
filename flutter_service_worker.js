630f143033cae1789cfabd417186aea9b9c7d06b
zepcp <vieirapereira@protonmail.com>
2020-04-12 11:06:41 +0100

    new icon

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -1,15 +1,16 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "/index.html": "4934c2de71727ef1c40d7f0b23371e13",
-"/main.dart.js": "4b60981b65db66a2bc59080a46f174ed",
+  "/index.html": "b3f13f686707e600519ac93818e6e127",
+"/app_icon.jpg": "8fe7131d104c1d46dbeec473364425fa",
+"/main.dart.js": "0409e4c9ddb5410eea2d740db5030c4d",
 "/favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "/manifest.json": "73974f27b05f20b4cb13d13177ec9f50",
 "/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/LICENSE": "039f898f49fc67ef86eb1dfbbcaddb62",
+"/assets/LICENSE": "d6266310754b45e3bb6d8a7b304a8cdc",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
