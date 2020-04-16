e6c408a2f6ab8c5fa93654f96a3fc61b1d446ed1
zepcp <vieirapereira@protonmail.com>
2020-04-16 21:32:15 +0100

    appcexplorer mockup

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "/index.html": "cd6ef4ff837c9fb9e0da59e7830b219a",
 "/app_icon.jpg": "8fe7131d104c1d46dbeec473364425fa",
-"/main.dart.js": "e9e382eb5800c098e0c8a20e948bbe4c",
+"/main.dart.js": "b698ae56c55853b7ef21e90343fa306d",
 "/manifest.json": "e0a8c1565eb5b6466e815857091fe48c",
 "/assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
 "/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
-"/assets/LICENSE": "073679045b3f44b47c261c97843dc832",
+"/assets/LICENSE": "94e1cd01bd574d22f1615490f145a2ac",
 "/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
 "/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16"
 };
